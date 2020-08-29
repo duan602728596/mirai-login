e9394f595abadb6deece25fc582ee2a3bd64d1be
段昊辰 <duanhaochen@126.com>
2020-08-29 20:18:03 +0800

    build: 重命名和添加版本号文件

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,9 @@
 const util = require('util');
+const { promises: fs } = require('fs');
 const glob = require('glob');
 const path = require('path');
 const fse = require('fs-extra');
+const { version } = require('../lerna.json');
 
 const globPromise = util.promisify(glob);
 
@@ -10,7 +12,7 @@ const build = path.join(cwd, 'build');
 
 async function clean() {
   // 删除mac
-  const macFiles = await globPromise(path.join(build, 'mac/mac/mirai-login/Contents/Resources/*.lproj'));
+  const macFiles = await globPromise(path.join(build, 'mac/mac/mirai-login.app/Contents/Resources/*.lproj'));
   const macDeleteTasks = [];
 
   macFiles.forEach((o) => !/zh_CN/i.test(o) && macDeleteTasks.push(fse.remove(o)));
@@ -24,6 +26,14 @@ async function clean() {
   winFiles.forEach((o) => !/zh-CN/i.test(o) && winDeleteTasks.push(fse.remove(o)));
 
   await Promise.all(winDeleteTasks);
+
+  // 写入版本号
+  await fs.writeFile(path.join(build, 'mac/mac/version'), `v${ version }`);
+  await fs.writeFile(path.join(build, 'win/win-unpacked/version'), `v${ version }`);
+
+  // 重命名
+  await fs.rename(path.join(build, 'mac/mac'), path.join(build, `mac/mirai-login-v${ version }-mac`));
+  await fs.rename(path.join(build, 'win/win-unpacked'), path.join(build, `win/mirai-login-v${ version }-win`));
 }
 
 clean();
